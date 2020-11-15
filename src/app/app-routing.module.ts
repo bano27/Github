28829833d9d68b3b5a8fbc8a0961993432366e3f
bano27 <thairukibe2798@gmail.com>
2020-11-15 23:41:43 +0300

    added html content in search-user and repositories components, before generating a Users and Repos services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchUserComponent } from "./search-user/search-user.component";
+import { RepositoriesComponent } from "./repositories/repositories.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search-user', component: SearchUserComponent },
+  { path: 'repositories', component: RepositoriesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
